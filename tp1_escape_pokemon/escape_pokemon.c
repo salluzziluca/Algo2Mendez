9143f859fabb11b8713cb8dcaaa3fb507faf8f35
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-03-23 09:43:40 -0300

    Cargar las consignas del TP1

---
+++ tp1_escape_pokemon/escape_pokemon.c
@@ -0,0 +1,30 @@
+#include "src/sala.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	//Los archivos deben venir como parámetros del main
+	sala_t *sala = sala_crear_desde_archivos("???", "???");
+
+	if (sala == NULL) {
+		printf("Error al crear la sala de escape\n");
+		return -1;
+	}
+
+	//Mostrar todos los objetos en la sala
+
+	printf("Objetos...\n");
+
+	//Mostrar si son válidas las siguientes interacciones
+	//1. examinar habitacion
+	//2. abrir pokebola
+	//3. usar llave cajon
+	//4. quemar mesa
+
+	printf("Interacciones...\n");
+
+	sala_destruir(sala);
+
+	return 0;
+}
