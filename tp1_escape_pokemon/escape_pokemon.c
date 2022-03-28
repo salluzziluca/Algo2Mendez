9ddc8c312ab4b765b4ae482442fd7925adaa9d75
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-03-28 18:31:07 -0300

    Pasar los archivos de texto por ref al main

--- tp1_escape_pokemon/escape_pokemon.c
+++ tp1_escape_pokemon/escape_pokemon.c
@@ -1,12 +1,20 @@
 #include "src/sala.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[])
 {
+
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	
+	/*
 	//Los archivos deben venir como parámetros del main
-	sala_t *sala = sala_crear_desde_archivos("objetos.txt", "interacciones.txt");
+	sala_t *sala = sala_crear_desde_archivos(argv[0], argv[1]);
 
 	if (sala == NULL) {
 		printf("Error al crear la sala de escape\n");
@@ -24,7 +32,7 @@ int main(int argc, char *argv[])
 
 	printf("Interacciones...\n");
 
-	//sala_destruir(sala);
+	//sala_destruir(sala);*/
 
 	return 0;
 }
