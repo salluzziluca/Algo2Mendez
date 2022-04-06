6e1dcaea8ec86f62cef1daa4fa400f4fe5ca04d0
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-04-05 21:16:31 -0300

    Compila pero no se guarda el 2do objeto

--- tp1_escape_pokemon/src/sala.c
+++ tp1_escape_pokemon/src/sala.c
@@ -5,28 +5,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 #define LARGO_MAX_LINEA 1024
-#define LARGO_MAX_BOOL 5
+#define LARGO_MAX_BOOL 15
 
-struct objeto *agregar_objeto_a_vector(struct objeto ***vector, int *tamanio, struct objeto *objeto)
+
+struct objeto **agregar_objeto_a_vector(struct objeto **vector, int *tamanio, struct objeto *objeto)
 {
-	struct objeto *bloque_vector_objetos = realloc(**vector,((((unsigned long)(*tamanio)) + 1) * sizeof(struct objeto)));
-	
+	struct objeto **bloque_vector_objetos = realloc(vector,((((unsigned long)(*tamanio)) + 1) * sizeof(struct objeto)));
+
 	if (bloque_vector_objetos == NULL)
 			return NULL;
 
-	bloque_vector_objetos[*tamanio] = *objeto;
+	(bloque_vector_objetos)[*tamanio]= objeto;
 	(*tamanio)++;
 
 	return bloque_vector_objetos;
+}
 
 
-
-}	
 sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones)
 {
-	struct sala sala;
-	sala.cantidad_objetos=0;
-
+	struct sala *sala= malloc( sizeof(struct sala));
+	sala->objetos = calloc(1, sizeof(struct objetos*));
 	
 	FILE *archivo_objetos= fopen(objetos, "r");
 
@@ -41,13 +40,13 @@ sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones
 		return NULL;
 		
 	struct objeto *objeto_a_agregar = objeto_crear_desde_string(linea);
-	sala.objetos=NULL;
 
-	agregar_objeto_a_vector(&sala.objetos, &sala.cantidad_objetos, objeto_a_agregar);
-
+	sala->objetos= agregar_objeto_a_vector(sala->objetos, &sala->cantidad_objetos, objeto_a_agregar);
+	//TODO ver por que no se guarda el segundo objeto
 	while (linea_leida){	
 		linea_leida = fgets(linea, LARGO_MAX_LINEA, archivo_objetos);
-		objeto_a_agregar=objeto_crear_desde_string(linea);
+		struct objeto *objeto_a_agregar =objeto_crear_desde_string(linea);
+		sala->objetos= agregar_objeto_a_vector(sala->objetos, &sala->cantidad_objetos, objeto_a_agregar);
 	}
 	
 	fclose(archivo_objetos);
