ea1c3aa67debce2445a377a650e8181604abc5a6
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-04-06 18:34:45 -0300

    Cargar los los objetos adecuadamente por memoria

--- tp1_escape_pokemon/src/sala.c
+++ tp1_escape_pokemon/src/sala.c
@@ -8,20 +8,21 @@
 #define LARGO_MAX_BOOL 15
 
 
-struct objeto **agregar_objeto_a_vector(struct objeto **vector, int *cantidad_objetos, struct objeto *objeto_actual)
+
+
+int agregar_objeto_a_vector(struct objeto ***objetos, int *cantidad_objetos, struct objeto *objeto_actual)
 {
-	struct objeto **bloque_vector_objetos = realloc(vector,((((unsigned long)(*cantidad_objetos)) + 1) * sizeof(struct objeto)));
+	struct objeto **bloque_auxiliar = realloc(*objetos, ((unsigned)(*cantidad_objetos)+1) * sizeof(struct objeto*));
 
-	if (bloque_vector_objetos == NULL)
-			return NULL;
+	if(bloque_auxiliar == NULL)
+		return -1;
 
-	(bloque_vector_objetos)[*cantidad_objetos]= objeto_actual;
+	*objetos= bloque_auxiliar;
+	bloque_auxiliar[*cantidad_objetos]=objeto_actual;
 	(*cantidad_objetos)++;
 
-	return bloque_vector_objetos;
+	return 0;
 }
-
-
 
 sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones)
 {
@@ -42,14 +43,17 @@ sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones
 		
 	struct objeto *objeto_a_agregar = objeto_crear_desde_string(linea);
 
-	sala->objetos= agregar_objeto_a_vector(sala->objetos, &sala->cantidad_objetos, objeto_a_agregar);
+	agregar_objeto_a_vector(&sala->objetos, &sala->cantidad_objetos, objeto_a_agregar);
 	//TODO ver por que no se guarda el segundo objeto
 	while (linea_leida){	
 		linea_leida = fgets(linea, LARGO_MAX_LINEA, archivo_objetos);
 		struct objeto *objeto_a_agregar =objeto_crear_desde_string(linea);
-		sala->objetos= agregar_objeto_a_vector(sala->objetos, &sala->cantidad_objetos, objeto_a_agregar);
+		agregar_objeto_a_vector(&sala->objetos, &sala->cantidad_objetos, objeto_a_agregar);
 	}
-	
+	for (int i = 1; i < sala->cantidad_objetos-1; ++i)
+	{
+		printf("%s\n", sala->objetos[i]->nombre);
+	}
 	fclose(archivo_objetos);
 	/*FILE *interacciones = fopen(interacciones, "r");*/
 
