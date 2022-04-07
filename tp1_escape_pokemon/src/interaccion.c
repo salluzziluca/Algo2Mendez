473a54cd014eeecbd2a328cce40f42fc3b8af7fd
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2022-04-07 19:57:48 -0300

    implementar la funcion crear interaccion

--- tp1_escape_pokemon/src/interaccion.c
+++ tp1_escape_pokemon/src/interaccion.c
@@ -2,16 +2,34 @@
 #include "interaccion.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX_ENUM 2
 struct interaccion *interaccion_crear_desde_string(const char *string)
 {
 	struct interaccion *interaccion_actual= malloc(sizeof(struct interaccion));
-	char valor_enum[MAX_ENUM];
-	sscanf(string,"%[^;];%[^;];%[^;];%[^:]:%[^:]:%[^\n]\n", interaccion_actual->objeto, interaccion_actual->verbo,
-	interaccion_actual->objeto_parametro, valor_enum,interaccion_actual->accion.objeto,
+
+	char *tipo_accion_actual=malloc(sizeof(char));
+	enum tipo_accion;
+	sscanf(string,"%[^;];%[^;];%[^;];%c:%[^:]:%[^\n]\n", interaccion_actual->objeto, interaccion_actual->verbo,
+	interaccion_actual->objeto_parametro, tipo_accion_actual,interaccion_actual->accion.objeto,
 	interaccion_actual->accion.mensaje
 	);
-	int valor_enum_int=(int)valor_enum[0];
-	
-	return NULL;
+
+	switch (*tipo_accion_actual)
+	{
+	case 'd':
+		interaccion_actual->accion.tipo=DESCUBRIR_OBJETO;
+		break;
+	case 'r':
+		interaccion_actual->accion.tipo=REEMPLAZAR_OBJETO;
+		break;
+	case 'e':
+		interaccion_actual->accion.tipo=ELIMINAR_OBJETO;
+		break;
+	case 'm':
+		interaccion_actual->accion.tipo=DESCUBRIR_OBJETO;
+		break;	
+	default:
+		break;
+	}
+
+	return interaccion_actual;
 }
