589b5602a3fc75f2fc1589618b4635ac20afd19f
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-04-10 11:30:09 -0300

    Poner bonito objetos

--- tp1_escape_pokemon/src/objeto.c
+++ tp1_escape_pokemon/src/objeto.c
@@ -13,9 +13,10 @@ struct objeto *objeto_crear_desde_string(const char *string)
 	char nombre_aux[MAX_NOMBRE] = "\0";
 	char descripcion_aux[MAX_TEXTO] = "\0";
 	char bool_aux[MAX_BOOL] = "\0";
+
 	if(string ==NULL)
 		return NULL;
-	else if((strcmp(string,"_")==0)){
+	else if((strcmp(string,"_") == 0)){
 		strcpy(objeto_actual->nombre,"\0");
 		strcpy(objeto_actual->descripcion,"\0");
 		objeto_actual->es_asible=false;
@@ -24,13 +25,14 @@ struct objeto *objeto_crear_desde_string(const char *string)
 		sscanf(string,"%[^;];%[^;];%[^\n]\n",nombre_aux, descripcion_aux, bool_aux);
 		
 		validar_y_agregar_campos_objeto(nombre_aux, objeto_actual->nombre, objeto_actual);
+
 		validar_y_agregar_campos_objeto(descripcion_aux, objeto_actual->descripcion, objeto_actual);
 
-		if(strcmp(bool_aux,"true")==0)
-			objeto_actual->es_asible=true;
-		else if ((strcmp(bool_aux,"false")==0))
+		if(strcmp(bool_aux,"true") == 0)
+			objeto_actual->es_asible = true;
+		else if ((strcmp(bool_aux,"false") == 0))
 		{
-			objeto_actual->es_asible=false;
+			objeto_actual->es_asible = false;
 		}
 		else
 		{
@@ -38,5 +40,5 @@ struct objeto *objeto_crear_desde_string(const char *string)
 		}
 	}	
 
-	return (objeto_actual);
+	return objeto_actual;
 }
