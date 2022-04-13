795be949b961e605c4c3b254b27efc25592845c8
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-04-12 22:22:22 -0300

    Agregar los archivos de tda_lista

---
+++ tda_lista/pruebas.c
@@ -0,0 +1,12 @@
+#include "src/lista.h"
+#include "src/pila.h"
+#include "src/cola.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pa2m.h"
+
+int main() {
+  pa2m_nuevo_grupo("Pruebas y mas pruebas");
+
+  return pa2m_mostrar_reporte();
+}
