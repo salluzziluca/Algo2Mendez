2297c7e04d4250482754fd73106f98c4a6a1a5a7
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-04-23 12:57:26 -0300

    Modularizar pruebas basicas de insercción

--- tda_lista/pruebas.c
+++ tda_lista/pruebas.c
@@ -5,15 +5,34 @@
 #include <stdlib.h>
 #include "pa2m.h"
 
-int main() {
-  pa2m_nuevo_grupo("Pruebas y mas pruebas");
-  //TODO: crear una prueba que me verifique la inserccion correcta de un elemento
+void pruebas_de_inserccion_de_objeto()
+{
   char a = 'a';
+
   lista_t *lista = lista_crear();
-  pa2m_afirmar(lista != NULL, "se puede crear una lista");
+
+  pa2m_afirmar(lista != NULL, "Se puede crear una lista");
   lista_insertar(lista, &a);
-  pa2m_afirmar(lista->nodo_inicio->elemento == &a, "se puede insertar un elemento en la lista");
+  pa2m_afirmar(lista->nodo_inicio->elemento == &a, "Se puede insertar un elemento en la lista");
   pa2m_afirmar(lista->nodo_fin->siguiente == NULL, "El ultimo elemento apunta a NULL");
 
+  char b = 'b';
+
+  lista_insertar(lista, &b);
+  pa2m_afirmar(lista->nodo_inicio->siguiente->elemento == &b, "El primer elemento apunta al segundo");
+
+  char c = 'c';
+  
+  lista_insertar(lista, &c);
+  pa2m_afirmar(lista->nodo_inicio->siguiente->siguiente->elemento == &c, "El segundo elemento apunta al tercero");
+  pa2m_afirmar(lista->cantidad == 3, "La cantidad de elementos es 3");
+}
+
+int main() {
+  pa2m_nuevo_grupo("Pruebas de insercción");
+  pruebas_de_inserccion_de_objeto();
+
+  pa2m_nuevo_grupo("Pruebas y mas pruebas");
+  
   return pa2m_mostrar_reporte();
 }
