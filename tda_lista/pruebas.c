2b30c9694b12bf6ae483d0c47a6bb6b35ddd8353
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-04-23 12:57:26 -0300

    iniciar implementacion prueba de inserccion

--- tda_lista/pruebas.c
+++ tda_lista/pruebas.c
@@ -7,6 +7,12 @@
 
 int main() {
   pa2m_nuevo_grupo("Pruebas y mas pruebas");
+  //TODO: crear una prueba que me verifique la inserccion correcta de un elemento
+  char a = 'a';
+  lista_t *lista1 = lista_crear();
+  pa2m_afirmar(lista, "se puede crear una lista");
+  lista_insertar(lista, &a);
+  pa2m_afirmar(lista->nodo_inicio->elemento == &a, "se puede insertar un elemento en la lista");
 
   return pa2m_mostrar_reporte();
 }
