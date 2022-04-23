c86c0e06c9b9fc0e739d5f5c635d72183ad7bad0
SalluzziLuca <63552496+salluzziluca@users.noreply.github.com>
2022-04-23 12:57:26 -0300

    Crear prueba que verifique inserccion de elemento

--- tda_lista/pruebas.c
+++ tda_lista/pruebas.c
@@ -9,8 +9,8 @@ int main() {
   pa2m_nuevo_grupo("Pruebas y mas pruebas");
   //TODO: crear una prueba que me verifique la inserccion correcta de un elemento
   char a = 'a';
-  lista_t *lista1 = lista_crear();
-  pa2m_afirmar(lista, "se puede crear una lista");
+  lista_t *lista = lista_crear();
+  pa2m_afirmar(lista != NULL, "se puede crear una lista");
   lista_insertar(lista, &a);
   pa2m_afirmar(lista->nodo_inicio->elemento == &a, "se puede insertar un elemento en la lista");
 
