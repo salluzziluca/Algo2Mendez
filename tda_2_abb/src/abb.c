fe27d4739a29d3ef69094ee351baa21f5e7ad556
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2022-05-10 12:49:45 -0300

    Empezar la implementacion de abb_recorrer

--- tda_2_abb/src/abb.c
+++ tda_2_abb/src/abb.c
@@ -2,6 +2,9 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#define INORDEN 0
+#define PREORDEN 1
+#define POSTODEN 2
 
 abb_t *abb_crear(abb_comparador comparador)
 {
@@ -19,15 +22,19 @@ abb_t *abb_crear(abb_comparador comparador)
 	arbol->tamanio = 0;
 	return arbol;
 }
-
+/*
+* Recibe un nodo, un elemento a agregar y una forma de compararls. Si el nod es nulo, 
+* lo crea. Si es nodo no tiene elemento, le asigna el elemento actual.
+* Por ultimo, si el nodo tiene elemento, lo compara con el actual y
+* segun si es la comparación devuelve mayor, menor o igual a 0,se desplaza hacia
+* el siguiente nodo en el arbol. Nodo derecho o izquierdo respectivaemente.
+*/
 nodo_abb_t *nodo_insertar(nodo_abb_t *nodo, void *elemento, abb_comparador comparador){
 	if (nodo == NULL){
 		nodo_abb_t *nodo = calloc(1, sizeof(nodo_abb_t));
 		if (nodo == NULL){
 			return NULL;
 		}
-		nodo->elemento = elemento;
-		return nodo;
 	}
 
 	if(nodo->elemento == NULL){
@@ -63,6 +70,12 @@ void *abb_quitar(abb_t *arbol, void *elemento)
 {
 	return NULL;
 }
+/*
+* Itera recursivamente por el arbol comparando el elemento igresado por parametro con 
+* el elemento del nodo actual. Si el comparador devuelve 0, los elementos son iguales
+* y la funcion lo duelve. De lo contrario, itera al proximo nodo. Al derecho si
+* la comparacion devuelve mayor a cero o al derecho si devuelve menor a cero. 
+*/
 void *nodo_buscar(nodo_abb_t *nodo, void *elemento, abb_comparador comparador){
 	if(nodo == NULL)
 		return NULL;
@@ -127,8 +140,33 @@ size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
 	return 0;
 }
 
+size_t nodo_recorrer(nodo_abb_t *nodo, abb_recorrido recorrido, void **array,
+		    size_t tamanio_array, size_t elementos_recorridos)
+{
+	
+
+	
+	switch (recorrido){
+	case INORDEN:
+		elementos_recorridos = nodo_recorrer(nodo->izquierda, recorrido, array, tamanio_array, elementos_recorridos);
+		array[elementos_recorridos] = nodo->elemento;
+		(elementos_recorridos)++;
+		elementos_recorridos = nodo_recorrer(nodo->derecha, recorrido, array, tamanio_array, elementos_recorridos);
+		break;
+	
+	default:
+		break;
+	}
+	return elementos_recorridos;	
+}
 size_t abb_recorrer(abb_t *arbol, abb_recorrido recorrido, void **array,
 		    size_t tamanio_array)
 {
-	return 0;
+	if(arbol == NULL)
+		return 0;
+
+	size_t elementos_recorridos = 0;
+	elementos_recorridos = nodo_recorrer(arbol->nodo_raiz, recorrido, array, tamanio_array, elementos_recorridos);
+	return elementos_recorridos;
+	
 }
