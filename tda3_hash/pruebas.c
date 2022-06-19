6fada26c484db4da093679bf41ccc4f457154b7d
Luca <salluzzi.luca@gmail.com>
2022-06-19 20:19:46 -0300

    Agregar pruebas y casos borde

--- tda3_hash/pruebas.c
+++ tda3_hash/pruebas.c
@@ -40,8 +40,6 @@ void hash_crear_crea_e_inicializa_correctamente(){
 	pa2m_afirmar(hash_cantidad(hash)== 0, "se crea el hash con cantidad 0");
 	pa2m_afirmar(hash->tabla != NULL, "se crea el hash con tabla disponibles");
 	hash_destruir(hash);
-	
-
 }
 
 void hash_insertar_inserta_correctamente(){
@@ -57,42 +55,67 @@ void hash_insertar_inserta_correctamente(){
 	pa2m_afirmar(*(int*)hash_obtener(hash, "hola") == 1, "Se inserta el par correctamente");
 	pa2m_afirmar(hash_cantidad(hash)== 1, "Se aumenta la cantidad correctamente");	
 	pa2m_afirmar(anterior == NULL, "No tenia elemento anterior");
+
 	pa2m_afirmar(hash_insertar(hash, "hola",&dos , &anterior) != NULL, "Se inserta correctamente");
+
 	pa2m_afirmar(*(int*)hash_obtener(hash, "hola") == 2, "Se sobreescribio el valor correctamente");
 	pa2m_afirmar(*(int*) anterior == 1, "El elemento sobrescrito es almacenado correctamente");
 	pa2m_afirmar(hash_cantidad(hash)== 1, "La cantidad no aumenta porque el elemento fue sobrescrito");
+
 	hash_quitar(hash, "hola");
 	hash_insertar(hash, "hola",&dos , &anterior);
+
 	pa2m_afirmar(*(int*)hash_obtener(hash, "hola") == 2, "Se puede agregar, eliminar y volver a agregar el mismo elemento con la misma clave");
+
 	pa2m_afirmar(hash_insertar(hash, "papa",&dos , &anterior) != NULL, "Se puede insertar dos veces el mismo elemento con diferente clave");
 	pa2m_afirmar((*(int *)hash_obtener(hash, "papa") == 2 )&& (*(int *)hash_obtener(hash, "hola") == 2), "Ambas claves tienen el mismo elemento");
+
 	pa2m_afirmar(hash_insertar(hash, NULL,&dos , &anterior) == NULL, "No se puede insertar par con clave nula");
+
 	pa2m_afirmar(hash_insertar(hash, "tres", &tres, &anterior) != NULL, "Se puede insertar un tercer par");
+
 	pa2m_afirmar(hash_obtener(hash, "tres") == &tres, "Se obtiene el valor correctamente");
-	pa2m_afirmar(hash_contiene(hash, "tres"), "contiene esa clave");
+	pa2m_afirmar(hash_contiene(hash, "tres"), "el hash contiene esa clave");
 	pa2m_afirmar(hash_cantidad(hash)== 3, "Se aumenta la cantidad correctamente");
 	pa2m_afirmar(anterior == NULL, "El elemento anterior es el correcto");
+
 	pa2m_afirmar(hash_insertar(hash, "cuatro", &cuatro, &anterior) != NULL, "Se puede insertar un cuarto par");
+
 	pa2m_afirmar(hash_obtener(hash, "cuatro") == &cuatro, "Se obtiene el valor correctamente");
 	pa2m_afirmar(hash_contiene(hash, "cuatro"), "contiene esa clave");
 	pa2m_afirmar(hash_cantidad(hash)== 4, "Se aumenta la cantidad correctamente");
+
 	pa2m_afirmar(hash_insertar(hash, "cinco", &cinco, &anterior) != NULL, "Se puede insertar un quinto par");
+
 	pa2m_afirmar(hash_obtener(hash, "cinco") == &cinco, "Se obtiene el valor correctamente");
 	pa2m_afirmar(hash_contiene(hash, "cinco"), "contiene esa clave");
 	pa2m_afirmar(hash_cantidad(hash)== 5, "Se aumenta la cantidad correctamente");
+
 	pa2m_afirmar(hash_insertar(hash, "seis", &seis, &anterior) != NULL, "Se puede insertar un sexto par");
+
 	pa2m_afirmar(hash_obtener(hash, "seis") == &seis, "Se obtiene el valor correctamente");
 	pa2m_afirmar(hash_contiene(hash, "seis"), "contiene esa clave");
 	pa2m_afirmar(hash_cantidad(hash)== 6, "Se aumenta la cantidad correctamente");
+
 	pa2m_afirmar(hash_insertar(hash, "siete", &siete, &anterior) != NULL, "Se puede insertar un septimo par");
+
 	pa2m_afirmar(hash_obtener(hash, "siete") == &siete, "Se obtiene el valor correctamente");
 	pa2m_afirmar(hash_contiene(hash, "siete"), "contiene esa clave");
 	pa2m_afirmar(hash_cantidad(hash)== 7, "Se aumenta la cantidad correctamente");
 
 	pa2m_afirmar(hash_insertar(hash, "Nulo", NULL, &anterior) != NULL, "se puede insertar un elemento nulo");
+
 	pa2m_afirmar(hash_contiene(hash, "Nulo") == true, "contiene esa clave");
 	pa2m_afirmar(hash_obtener(hash, "Nulo") == NULL, "Se obtiene el valor correctamente");
 	pa2m_afirmar(hash_cantidad(hash)== 8, "Se aumenta la cantidad correctamente");
+
+	hash_destruir(hash);
+
+	hash = hash_crear(3);
+	anterior = NULL;
+	pa2m_afirmar(hash_insertar(hash, "uno",&uno , &anterior) != NULL, "Se inserta <uno,1>");
+	pa2m_afirmar(hash_insertar(hash, "uno", &uno, &anterior) != NULL, "Se inserta <uno,1>");
+	pa2m_afirmar(*(int *)anterior == uno, "Se puede insertar dos veces el mismo par, genera que se sobreescriba el valor por el mismo que fue quitado");
 
 	hash_destruir(hash);
 }
@@ -102,19 +125,29 @@ void hash_quitar_quita_correctamente(){
 	pa2m_afirmar(hash_quitar(hash, "hola") == NULL, "No se puede quitar un par en un hash nulo");
 	hash = hash_crear(6);
 	void *anterior = NULL;
+
 	hash_insertar(hash, "hola", &uno, &anterior);
 	pa2m_afirmar(hash_quitar(hash, NULL) == NULL, "No se puede quitar un par pasando una clave nula");
 	pa2m_afirmar(hash_contiene(hash, "hola") == true, "El elemento existe");
 	pa2m_afirmar(hash_cantidad(hash) == 1, "La cantidad es 1");
+
 	pa2m_afirmar(*(int*)hash_quitar(hash, "hola") == 1, "Se puede quitar el elemento");
 	pa2m_afirmar(hash_contiene(hash, "hola") == false,"El elemento no se encuentra en el hash");
 	pa2m_afirmar(hash_cantidad(hash) == 0, "La cantidad es 0");
+
 	pa2m_afirmar(hash_insertar(hash, "hola", &uno, &anterior) != NULL, "Se puede insertar hola, uno");
 	pa2m_afirmar(hash_insertar(hash, "hola", &dos, &anterior) != NULL, "Se puede insertar hola,dos");
+
 	pa2m_afirmar(*(int*)anterior == uno, "El anterior es 1");
+
 	pa2m_afirmar(hash_quitar(hash, "hola") == &dos, "quito el elemento dos");
+
 	pa2m_afirmar(hash_insertar(hash, "hola", &tres, &anterior) != NULL, "Se puede insertar hola,tres");
+
 	pa2m_afirmar(anterior == NULL, "El anterior es NULL");
+	
+	hash_insertar(hash, "nulo", NULL, &anterior);
+	pa2m_afirmar(hash_quitar(hash, "nulo") == NULL, "Se puede quitar un elemento nulo");
 
 
 	hash_destruir(hash);
@@ -131,11 +164,11 @@ void hash_con_cada_clave_itera_correctamente(){
 	hash_insertar(hash, "chau", &dos, &anterior);
 	void *aux = NULL;
 	pa2m_afirmar(hash_con_cada_clave(hash, no_hace_nada, aux) == 2, "El iterador funciona correctamente");
+	pa2m_afirmar(hash_con_cada_clave(hash, NULL, aux) == 0, "No se puede iterar una funcion nula");
 	hash_destruir(hash);
 
 	hash = hash_crear(10);
-	int tres = 3, cuatro = 4, cinco = 5, seis = 6, siete = 7, ocho = 8, nueve = 9, diez = 10;
-	hash_insertar(hash, "uno", &uno, &anterior);
+	int tres = 3, cuatro = 4, cinco = 5, seis = 6, siete = 7, ocho = 8, nueve = 9;
 	hash_insertar(hash, "dos", &dos, &anterior);
 	hash_insertar(hash, "tres", &tres, &anterior);
 	hash_insertar(hash, "cuatro", &cuatro, &anterior);
@@ -144,11 +177,8 @@ void hash_con_cada_clave_itera_correctamente(){
 	hash_insertar(hash, "siete", &siete, &anterior);
 	hash_insertar(hash, "ocho", &ocho, &anterior);
 	hash_insertar(hash, "nueve", &nueve, &anterior);
-	hash_insertar(hash, "diez", &diez, &anterior);
+	hash_insertar(hash, "diez", NULL, &anterior);
 	int contador = 0;
-	size_t recorridos = hash_con_cada_clave(hash, recorro_3_veces, &contador);
-	recorridos = recorridos;
-	contador = 0; 
 	pa2m_afirmar(hash_con_cada_clave(hash, recorro_3_veces, &contador) == 3, "El iterador recorre 3 veces");
 
 	hash_destruir(hash);
