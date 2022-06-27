db66d85377ed5f7ce2e0f98514fd7d20bfd36c6d
Luca <salluzzi.luca@gmail.com>
2022-06-27 14:07:59 -0300

    Declarar la estructura del jugador

--- tp2_escape_pokemon/src/estructura_sala.h
+++ tp2_escape_pokemon/src/estructura_sala.h
@@ -1,10 +1,21 @@
 #ifndef ESTRUCTURA_SALA_H
 #define ESTRUCTURA_SALA_H
 #include "hash.h"
+
+typedef struct jugador
+{
+	hash_t *objetos_conocidos;
+	size_t cantidad_objetos_conocidos;
+	hash_t *objetos_poseidos;
+	size_t cantidad_objetos_poseidos;
+} jugador_t;
 struct sala {
 	hash_t *objetos;
 	size_t cantidad_objetos;
 	hash_t *interacciones;
 	size_t cantidad_interacciones;
+	jugador_t *jugador;
 };
+
+
 #endif // ESTRUCTURA_SALA_H
