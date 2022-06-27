8e7a67653db7cd23d7a59017fdc3b9403d67dd86
Luca <salluzzi.luca@gmail.com>
2022-06-27 14:12:00 -0300

    Agregar espacio para mejorar legibilidad

--- tp2_escape_pokemon/src/estructura_sala.h
+++ tp2_escape_pokemon/src/estructura_sala.h
@@ -9,6 +9,7 @@ typedef struct jugador
 	hash_t *objetos_poseidos;
 	size_t cantidad_objetos_poseidos;
 } jugador_t;
+
 struct sala {
 	hash_t *objetos;
 	size_t cantidad_objetos;
@@ -17,15 +18,5 @@ struct sala {
 	jugador_t *jugador;
 };
 
-<<<<<<< HEAD
-=======
-typedef struct jugador
-{
-	hash_t *objetos_conocidos;
-	size_t cantidad_objetos_conocidos;
-	hash_t *objetos_poseidos;
-	size_t cantidad_objetos_poseidos;
-} jugador_t;
->>>>>>> 8e3a4cc2d62aeff8aa0412ea8558773d57340cb5
 
 #endif // ESTRUCTURA_SALA_H
